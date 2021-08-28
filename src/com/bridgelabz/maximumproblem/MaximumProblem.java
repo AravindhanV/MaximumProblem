3058658d1f4c00bc775807c16b3dd9f20e65333c
Aravindhan V <aravindhan.v@perfios.com>
2021-08-28 22:16:56 +0530

    ADDED: Logic to calculate max of 3 Strings

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -4,6 +4,7 @@ public class MaximumProblem {
 	public static void main(String[] args) {
 		getMaximum(1,2,3);
 		getMaximum(0.1f,0.2f,0.3f);
+		getMaximum("Apple","Peach","Banana");
 	}
 	
 	public static void getMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
@@ -34,6 +35,22 @@ public class MaximumProblem {
 				System.out.println(secondNumber.floatValue());
 			} else {
 				System.out.println(thirdNumber.floatValue());
+			}
+		}
+	}
+	
+	public static void getMaximum(String firstWord, String secondWord, String thirdWord) {
+		if (firstWord.compareTo(secondWord) > 0) {
+			if (firstWord.compareTo(thirdWord) > 0) {
+				System.out.println(firstWord);
+			} else {
+				System.out.println(thirdWord);
+			}
+		} else {
+			if (secondWord.compareTo(thirdWord) > 0) {
+				System.out.println(secondWord);
+			} else {
+				System.out.println(thirdWord);
 			}
 		}
 	}
