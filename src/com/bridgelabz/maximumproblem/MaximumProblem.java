8a86f788cc8cf672e6295520dfb53ecf490d32c8
Aravindhan V <aravindhan.v@perfios.com>
2021-08-28 21:52:48 +0530

    ADDED: Logic to calculate max of 3 numbers implemented

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -1,5 +1,23 @@
 package com.bridgelabz.maximumproblem;
 
 public class MaximumProblem {
-	
+	public static void main(String[] args) {
+		Integer firstNumber = new Integer(1);
+		Integer secondNumber = new Integer(3);
+		Integer thirdNumber = new Integer(2);
+
+		if (firstNumber.compareTo(secondNumber) > 0) {
+			if (firstNumber.compareTo(thirdNumber) > 0) {
+				System.out.println(firstNumber.intValue());
+			} else {
+				System.out.println(thirdNumber.intValue());
+			}
+		} else {
+			if (secondNumber.compareTo(thirdNumber) > 0) {
+				System.out.println(secondNumber.intValue());
+			} else {
+				System.out.println(thirdNumber.intValue());
+			}
+		}
+	}
 }
