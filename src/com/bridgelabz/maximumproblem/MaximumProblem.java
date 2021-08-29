19259a46870d8abf8abf842ee0dbd27c823c724f
Aravindhan V <aravindhan.v@perfios.com>
2021-08-29 15:42:27 +0530

    REFACTOR: Generic class created instead of a generic method for getMaximum

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -1,13 +1,21 @@
 package com.bridgelabz.maximumproblem;
 
-public class MaximumProblem {
-	public static void main(String[] args) {
-		getMaximum(1,2,3);
-		getMaximum(0.1f,0.2f,0.3f);
-		getMaximum("Apple","Peach","Banana");
+public class MaximumProblem <E extends Comparable<E>>{
+	E first,second, third;
+	
+	public MaximumProblem(E first, E second, E third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
 	}
 	
-	public static <E extends Comparable<E>> void getMaximum(E first, E second, E third) {
+	public static void main(String[] args) {
+		new MaximumProblem<Integer>(1,2,3).getMaximum();
+		new MaximumProblem<Float>(0.1f,0.2f,0.3f).getMaximum();
+		new MaximumProblem<String>("Apple","Peach","Banana").getMaximum();
+	}
+	
+	public void getMaximum() {
 		if (first.compareTo(second) > 0) {
 			if (first.compareTo(third) > 0) {
 				System.out.println(first);
